e176ab546329d55718cfcb752e10fc9001dabfa8
LorenzoVarese <lorenzo.varese@usi.ch>
2023-10-21 08:33:44 +0200

    Add comments and fix naming convention

--- src/main/scala/ch/usi/si/msde/edsl/assignment01/model/WorkoutSemanticModel.scala
+++ src/main/scala/ch/usi/si/msde/edsl/assignment01/model/WorkoutSemanticModel.scala
@@ -1,123 +1,285 @@
 package ch.usi.si.msde.edsl.assignment01.model
 
-/** The types used to specify mass/weight (Mass) and to construct a weight in
-  * kilograms
-  */
+import scala.collection.immutable.Set
+
+/** Imports for mass and weight types. */
 import squants.mass.Mass
 import squants.mass.Kilograms
 
-import scala.collection.immutable.Set
-
-/** The types to be used to specify speed in Kms per hour (see example)
-  */
+/** Imports for speed types. */
 import squants.motion.KilometersPerHour
 import squants.motion.Velocity
 
-/** The types to be used to specify time in minutes
-  */
+/** Imports for time types. */
 import squants.time.Minutes
 import squants.time.Time
-/*
- * Implement here, or in other scala files in this package,
- * the semantic model for Exercise 2. If you use other
- * files, please remember to import them.
- */
-
 
 //<editor-fold desc="Named">
+
+/**
+ * A trait for named objects.
+ *
+ * @define name the name of the object
+ */
 sealed trait NamedObject:
   val name: Name
 
+/**
+ * Class representing a name.
+ *
+ * @constructor create a new name with a string value.
+ * @param value the string value of the name
+ */
 case class Name(value: String):
   require(value.nonEmpty)
+
 // </editor-fold>
 
-case class Weight(mass: Mass):
-  require(mass.value >= 0)
-
 //<editor-fold desc="Description">
+
+/**
+ * Class representing a description.
+ *
+ * @constructor create a new description with a string value.
+ * @param value the string value of the description
+ */
 case class Description(value: String):
   require(value.nonEmpty)
+
+/**
+ * A trait for describable objects.
+ *
+ * @define description the description of the object
+ */
 sealed trait DescObject:
   val description: Description
+
 // </editor-fold>
 
 //<editor-fold desc="Workout_Params">
+
+/**
+ * Class representing weight.
+ *
+ * @constructor create a new weight with a mass.
+ * @param mass the mass value of the weight
+ */
+case class Weight(mass: Mass):
+  require(mass.value >= 0)
+
+/**
+ * Class representing a duration.
+ *
+ * @constructor create a new duration with a time value.
+ * @param min the time value of the duration
+ */
 case class Duration(min: Time):
   require(min.toMinutes > 0)
 
+/**
+ * Class representing a workout set.
+ *
+ * @constructor create a new workout set with a value and repetition.
+ * @param value the number of sets
+ * @param repetition the number of repetitions in each set
+ */
 case class Workout_Set(value: Int, repetition: Repetition):
   require(value > 0)
 
+/**
+ * Class representing a repetition.
+ *
+ * @constructor create a new repetition with a value.
+ * @param value the number of repetitions
+ */
 case class Repetition(value: Int):
   require(value > 0)
+
 // </editor-fold>
 
 //<editor-fold desc="Workout">
+
+/**
+ * Class representing a daily workout.
+ *
+ * @constructor create a new daily workout with a description and set of workouts.
+ * @param description the description of the daily workout
+ * @param workouts the set of workouts for the day
+ */
 case class WorkoutDaily(description: Description, workouts: Set[Workout] = Set()) extends DescObject
+
+/**
+ * A trait for different types of workouts.
+ *
+ * @define facilities the required facilities for the workout
+ */
 sealed trait Workout extends DescObject:
   val facilities : Set[RequirementFacility]
 
-// Set[RequirementSet] = Set() ---> meaning it can be null
+/**
+ * Class representing a cardio training workout.
+ *
+ * @constructor Create a new cardio training workout with a description, duration, and set of requirements.
+ * @param description Description of the cardio training workout.
+ * @param duration Duration of the cardio training.
+ * @param facilities Optional set of required facilities.
+ * @param cardioReqs Optional set of cardio requirements.
+ */
 case class WorkoutCardioTraining(description: Description, duration: Duration, facilities: Set[RequirementFacility] = Set(), cardioReqs: Set[RequirementCardio] = Set()) extends Workout
+
+/**
+ * Class representing a weight training workout.
+ *
+ * @constructor Create a new weight training workout with a description, set, and set of requirements.
+ * @param description Description of the weight training workout.
+ * @param set Workout set specification.
+ * @param facilities Optional set of required facilities.
+ * @param weightReqs Optional set of weight training requirements.
+ */
 case class WorkoutWeightTraining(description: Description, set: Workout_Set, facilities: Set[RequirementFacility] = Set(), weightReqs: Set[RequirementWeightTraining] = Set()) extends Workout
 
 // </editor-fold>
 
-//<editor-fold desc="Requirement">
-sealed trait Requirement extends NamedObject
-case class RequirementFacility(name: Name, facility: ObjectFacility) extends Requirement
+//<editor-fold desc="Facility">
 
-//Todo: check if weight is needed
-// equipments can be empty if user want to
-case class RequirementWeightTraining(name: Name, equipments: Set[WeightEquipment] = Set(), weight: Weight) extends Requirement
+/**
+ * Trait representing a general object facility.
+ */
+sealed trait ObjectFacility extends NamedObject
 
-case class RequirementCardio(name: Name, cardioMachines: CardioMachine) extends Requirement
+/**
+ * Case class representing a gym.
+ *
+ * @constructor Create a new gym with a name and set of equipment.
+ * @param name The name of the gym.
+ * @param equipment Optional set of equipment available in the gym.
+ */
+case class Gym(name: Name, equipment: Set[Equipment] = Set()) extends ObjectFacility
+
+/**
+ * Case class representing a generic facility.
+ *
+ * @constructor Create a new facility with a name.
+ * @param name The name of the facility.
+ */
+case class Facility(name : Name) extends ObjectFacility
+
+/**
+ * Case class representing a fitness center.
+ *
+ * @constructor Create a new fitness center with a name and set of facilities.
+ * @param name The name of the fitness center.
+ * @param facilities Optional set of facilities available in the fitness center.
+ */
+case class FitnessCenter(name: Name, facilities: Set[ObjectFacility] = Set()) extends NamedObject
+
 //</editor-fold>
 
-//<editor-fold desc="Facility">
-sealed trait ObjectFacility extends NamedObject
-case class Gym(name: Name, equipment: Set[Equipment] = Set()) extends ObjectFacility
-case class Facility(name : Name) extends ObjectFacility
-case class FitnessCenter(name: Name, facilities: Set[ObjectFacility] = Set()) extends NamedObject
+//<editor-fold desc="Requirement">
+
+/**
+ * Trait for requirements.
+ */
+sealed trait Requirement extends NamedObject
+
+/**
+ * Class representing a facility requirement.
+ *
+ * @constructor Create a new facility requirement.
+ * @param name Name of the facility requirement.
+ * @param facility Specified facility object.
+ */
+case class RequirementFacility(name: Name, facility: ObjectFacility) extends Requirement
+
+/**
+ * Class representing a weight training requirement.
+ *
+ * @constructor Create a new weight training requirement.
+ * @param name Name of the weight training requirement.
+ * @param equipments Optional set of weight equipment.
+ * @param weight Specified weight.
+ */
+case class RequirementWeightTraining(name: Name, equipments: Set[WeightEquipment] = Set(), weight: Weight) extends Requirement
+
+/**
+ * Class representing a cardio requirement.
+ *
+ * @constructor Create a new cardio requirement.
+ * @param name Name of the cardio requirement.
+ * @param cardioMachines Specified cardio machine.
+ */
+case class RequirementCardio(name: Name, cardioMachines: CardioMachine) extends Requirement
+
 //</editor-fold>
 
 //<editor-fold desc="Equipment">
+
 sealed trait WeightEquipment
 sealed trait Equipment extends NamedObject
 case class Item(name: Name, weight: Weight) extends Equipment, WeightEquipment
 sealed trait Machine extends Equipment
 case class CardioMachine(name: Name, setting: CardioMachineSetting) extends Machine
 case class WeightMachine(name: Name, setting: WeightMachineSetting, weights: Set[Weight] = Set()) extends Machine, WeightEquipment
+
 //</editor-fold>
 
 //<editor-fold desc="Settings">
 
+/**
+ * Case class representing the slope setting of a cardio machine.
+ *
+ * @constructor Create a new Slope instance.
+ * @param value The value of the slope. Must be between -30 and 30 inclusive.
+ */
 case class Slope(value: Int):
   require(value >= -30 && value <= 30)
+
+/**
+ * Case class representing the speed setting of a cardio machine.
+ *
+ * @constructor Create a new Speed instance.
+ * @param vel The velocity of the machine. Must be between 0 and 60 inclusive.
+ */
 case class Speed(vel: Velocity):
   require(vel.value >= 0 && vel.value <= 60)
+
+/**
+ * Case class representing the settings for a cardio machine.
+ *
+ * @constructor Create a new CardioMachineSetting instance.
+ * @param slope The slope setting for the cardio machine.
+ * @param speed The speed setting for the cardio machine.
+ */
 case class CardioMachineSetting(slope: Slope, speed: Speed)
+
+/**
+ * Case class representing the settings for a weight machine.
+ *
+ * @constructor Create a new WeightMachineSetting instance.
+ * @param min The minimum mass setting for the weight machine.
+ * @param max The maximum mass setting for the weight machine. Must be greater than min.
+ */
 case class WeightMachineSetting(min: Mass, max: Mass):
   require(max.value > min.value)
 
+//</editor-fold>
 
-
-/** Construct - if you want - one or more workout examples examples here.
+/**
+ * Construct - if you want - one or more workout examples examples here.
   */
-// Cardio Training Workout
 
 val cardioMachineSetting = CardioMachineSetting(Slope(30), Speed(KilometersPerHour(30)))
 val cardioMachine1 = CardioMachine(Name("Cardio Machine"), cardioMachineSetting)
 val weightMachineSetting = WeightMachineSetting(Kilograms(10), Kilograms(100))
-val weightMachine1 = WeightMachine(Name("Weight machine"), weightMachineSetting)
-val item = Item(Name("Item"), Weight(Kilograms(30)))
+val weightMachine1 = WeightMachine(Name("Weight Machine"), weightMachineSetting)
+val item = Item(Name("Kettlebell"), Weight(Kilograms(30)))
 
 val gym = Gym(Name("Gym 1"), Set(cardioMachine1, weightMachine1, item))
-val fitnessFacility = Facility(Name("Fitness facility"))
+val fitnessFacility = Facility(Name("Fitness Facility"))
 
-val requirementFitness = RequirementFacility(Name("My Fitness center "), fitnessFacility)
+val requirementFitness = RequirementFacility(Name("My Fitness Center"), fitnessFacility)
 val requirementGym = RequirementFacility(Name("My Gym"), gym)
+
 @main def exampleWorkout(): Unit =
 
   // Cardio
@@ -129,9 +291,8 @@ val requirementGym = RequirementFacility(Name("My Gym"), gym)
   // Workout empty
   val workout_Set = Workout_Set(10, Repetition(10))
   val weightTrainingEmpty = WorkoutWeightTraining(Description("Weight training empty"), workout_Set)
+
   // Workout non empty
-
-
   val itemDumbbell = Item(Name("Dumbbell"), Weight(Kilograms(30)))
   val itemBodyWeight = Item(Name("Body Weight"), Weight(Kilograms(10)))
   val requirementWeightTraining = RequirementWeightTraining(Name("Weight training daily"), Set(itemDumbbell, itemBodyWeight), Weight(Kilograms(40)))
@@ -143,15 +304,15 @@ val requirementGym = RequirementFacility(Name("My Gym"), gym)
   println(exampleWorkout)
 
 @main def exampleFitnessCenter(): Unit =
-  val EmptyFitnessCenter = FitnessCenter(Name("Fitness Center A"))
-  println(EmptyFitnessCenter)
+  val emptyFitnessCenter = FitnessCenter(Name("Fitness Center A"))
+  println(emptyFitnessCenter)
   val fitnessCenter = FitnessCenter(Name("Fitness Center B"), Set(gym, fitnessFacility))
   println(fitnessCenter)
 
 @main def exampleGymOwnsEquipment(): Unit =
   println(gym)
 
-@main def Example1Prof() : Unit =
+@main def example1Prof() : Unit =
   val gymFacility = Gym(Name("Gym"))
   val swimmingPoolFacility = Facility(Name("Swimming pool"))
   val fitnessCenter = FitnessCenter(Name("Fitness Center"), Set(swimmingPoolFacility, gymFacility))
@@ -161,7 +322,7 @@ val requirementGym = RequirementFacility(Name("My Gym"), gym)
   val outdoorRunning = WorkoutCardioTraining(Description("running workout"), Duration(Minutes(30)))
   println(WorkoutDaily(Description("Workout Daily"), Set(swimmingWorkout, outdoorRunning)))
 
-@main def Example2Prof() : Unit =
+@main def example2Prof() : Unit =
   // Facility
   val gymFacility = Gym(Name("Gym 2"))
   val fitnessCenter = FitnessCenter(Name("Fitness Center"), Set(gymFacility))
@@ -180,8 +341,9 @@ val requirementGym = RequirementFacility(Name("My Gym"), gym)
   val shoulderWorkout = WorkoutWeightTraining(Description("Shoulder workout"), Workout_Set(4, Repetition(10)),Set(requirementFacility), Set(requirementShoulderPressMachine))
   println(WorkoutDaily(Description("Upper Body weight training"),Set(chestWorkout, shoulderWorkout, bicepsWorkout)))
 
-/** Examples to construct weight, speeds, etc. and of the Option data type.
-  */
+/**
+ * Examples to construct weight, speeds, etc. and of the Option data type.
+ */
 @main def datatypeExamples(): Unit =
   // A mass/weight
   val mass1: Mass = Kilograms(20)
